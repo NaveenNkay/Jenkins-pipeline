bfd3e0aa619f631cced91f14277bd7a8fb48c531
NaveenNkay <147308581+NaveenNkay@users.noreply.github.com>
2024-04-24 07:35:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "neeha-first-bucket"
+	bucket = "naveenkc-first-bucket"
 }
 
 resource "aws_s3_bucket" "bucket2" {
-    
+    	bucket = "naveenkc-second-bucket"
 }
 
 provider "aws" {
